b17a7d02a7e9de2e6fbf7ee64d3220be25af1ebc
Inuokko <leajoussaume64@gmail.com>
2023-09-13 16:37:00 +0200

    Ajout page article + template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <script setup>
   import Header from './components/Header.vue';
   import Home from './views/Home.vue';
+  import Article from './views/Article.vue';
   import Footer from './components/Footer.vue';
-  import Preline from '../node_modules/preline/dist/preline.js';
+  // import Preline from '../node_modules/preline/dist/preline.js';
 </script>
 
 <template>
   <Header />
   <div>
     <Home />
-    <Footer />
   </div>
+  <Footer />
 </template>
 
 <style scoped>
 
 </style>
+
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue"
+import Article from "../views/Article.vue"
 import Contact from "../views/Contact.vue"
 
 const router = createRouter({
@@ -9,7 +10,7 @@ const router = createRouter({
         {path: "/contact", name: "Contact", component: Contact},
         {path: "/login", name: "Login", component: Login},
         {path: "/register", name: "Register", component: Register},
-        {path: "/articles", name: "Articles", component: Articles},
+        {path: "/article", name: "Article", component: Article},
 
     ],
     history: createWebHistory()
