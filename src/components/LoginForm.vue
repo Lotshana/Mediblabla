9654b77823e5484051d16530ba7eae8c3b9989d1
Alexandre BADOUX <badoux.al@gmail.com>
2023-09-15 12:27:02 +0200

    fin design connexion

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+    
         <div class="p-4 sm:p-7">
           <div class="text-center">
             <h1 class="block text-2xl font-bold text-darkblue">Connexion</h1>
-            <p class="mt-2 text-sm text-lightblue">
+            <p class="mt-2 text-sm text-darkblue">
               Vous n'avez pas de compte?
-              <a class="text-darkblue decoration-2 hover:underline font-medium" href="">
+              <a class="text-lightblue decoration-2 hover:underline font-medium" href="">
                 Inscription
               </a>
             </p>
@@ -17,34 +17,34 @@
                 <p v-if="errorMessage" class="(error A VOIR AVEC TAILWIND)"></p>
                 <!-- Form Group -->
                 <div>
-                  <label for="email" class="block text-sm mb-2 dark:text-white">Email</label>
+                  <label for="email" class="block text-sm mb-2 text-darkblue">Email</label>
                   <div class="relative">
-                    <input  type="email" id="email" v-model="email" name="email" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="email-error">
+                    <input  type="email" id="email" v-model="email" name="email" class="py-3 px-4 block w-full bg-lightgrey border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="email-error">
                     <div class=" absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
                       <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                       </svg>
                     </div>
                   </div>
-                  <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
+                  <p class="hidden text-xs text-red-600 mt-2" id="email-error">Merci d'entrer une adresse mail valide s'il vous plait</p>
                 </div>
                 <!-- End Form Group -->
     
                  <!-- Form Group -->
                  <div>
                   <div class="flex justify-between items-center">
-                    <label for="password" class="block text-sm mb-2 dark:text-white">Password</label>
-                    <a class="text-sm text-blue-600 decoration-2 hover:underline font-medium" href="../examples/html/recover-account.html">Forgot password?</a>
+                    <label for="password" class="block text-sm mb-2 text-darkblue">Mot de passe</label>
+                    <a class="text-sm text-blue-600 decoration-2 hover:underline font-medium" href="../examples/html/recover-account.html">Mot de passe oublié ?</a>
                   </div>
                   <div class="relative">
-                    <input type="password" v-model="password" id="password" name="password" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="password-error">
+                    <input type="password" v-model="password" id="password" name="password" class="py-3 px-4 block w-full  bg-lightgrey border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="password-error">
                     <div class=" absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
                       <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                       </svg>
                     </div>
                   </div>
-                  <p class="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p>
+                  <p class="hidden text-xs text-red-600 mt-2" id="password-error">8 caractères minimum</p>
                 </div>
                 <!-- End Form Group -->
       
@@ -54,7 +54,7 @@
                     <input id="remember-me" name="remember-me" type="checkbox" class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800">
                   </div>
                   <div class="ml-3">
-                    <label for="remember-me" class="text-sm text-lightblue">Remember me</label>
+                    <label for="remember-me" class="text-sm text-lightblue">Se souvenir de moi</label>
                   </div>
                 </div>
                 <!-- End Checkbox -->
@@ -65,7 +65,7 @@
 
         </div>
     </div>
-</div>
+
   </template>
   
   <script>
