888cacf24c9d631753b03ea097c5eaad45be15e5
Alexandre BADOUX <badoux.al@gmail.com>
2023-09-25 16:11:08 +0200

    creation article code finish
    a ton tour Lea

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
   import Header from './components/Header.vue';
-  import Connexion from './views/Connexion.vue';
- import Home from './views/Home.vue';
+ // import Connexion from './views/Connexion.vue';
+// import Home from './views/Home.vue';
   import Footer from './components/Footer.vue';
 
 </script>
