dca30f29530769d1269c0aca779acb4423140343
Lotshana <nahh.design@gmail.com>
2023-09-25 12:59:16 +0200

    Maj about

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,24 +11,23 @@
     <!-- End Col -->
 
     <div class="sm:order-1">
-      <h2 class="text-2xl font-bold md:text-3xl lg:text-4xl lg:leading-tight xl:text-5xl xl:leading-tight text-gray-800 dark:text-gray-200">
-        <a class="hover:text-blue-600 dark:text-gray-300 dark:hover:text-white" href="#">
-         L'équipe MediBlaBla !
-        </a>
+      <h2 class="text-2xl font-bold md:text-3xl lg:text-4xl lg:leading-tight xl:text-5xl xl:leading-tight text-darkblue">
+        L'équipe MediBlaBla !
       </h2>
 
       <h2 class="text-2xl font-bold md:text-3xl dark:text-white">Announcing a free plan for small teams</h2>
 
-      <p class="mb-5 inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quo, quia illum odio autem perspiciatis consectetur fuga a quod repellat aperiam vero delectus? Iusto alias, quam exercitationem libero reiciendis modi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea quo placeat provident illum blanditiis corporis exercitationem omnis explicabo impedit qui? Similique quia libero autem iure doloribus quidem commodi dolorem quam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel enim incidunt rerum assumenda. Porro quasi placeat ullam a cupiditate magni mollitia? Id, dolorum? Molestias possimus facere minima, asperiores culpa vero?
-      </p>
-      <p class="mb-5 inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
+      <p class="text-lg text-darkblue">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quo, quia illum odio autem perspiciatis consectetur fuga a quod repellat aperiam vero delectus? Iusto alias, quam exercitationem libero reiciendis modi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea quo placeat provident illum blanditiis corporis exercitationem omnis explicabo impedit qui? Similique quia libero autem iure doloribus quidem commodi dolorem quam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel enim incidunt rerum assumenda. Porro quasi placeat ullam a cupiditate magni mollitia? Id, dolorum? Molestias possimus facere minima, asperiores culpa vero?
       </p>
 
-      <p class="text-xs text-darkblue">
+      <!-- <p class="text-lg text-gray-800 dark:text-gray-200">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quo, quia illum odio autem perspiciatis consectetur fuga a quod repellat aperiam vero delectus? Iusto alias, quam exercitationem libero reiciendis modi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea quo placeat provident illum blanditiis corporis exercitationem omnis explicabo impedit qui? Similique quia libero autem iure doloribus quidem commodi dolorem quam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel enim incidunt rerum assumenda. Porro quasi placeat ullam a cupiditate magni mollitia? Id, dolorum? Molestias possimus facere minima, asperiores culpa vero?
+      </p> -->
+
+      <p class="text-xs text-darkblue py-24">
             Rédigé par l'équipe de MediBlaBla.
-          </p>
+      </p>
     </div>
   </div>
   <!-- End Grid -->
