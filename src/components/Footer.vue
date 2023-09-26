9b85783f5d8fd47edd34ef74b89da9cb2443d9f5
Lotshana <nahh.design@gmail.com>
2023-09-26 13:12:51 +0200

    Mis a jour header/footer responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
 <template>
  <footer class="py-12 bg-darkblue text-white flex justify-center text-center">
     <div class="flex flex-col justify-center">
-        <a href=""><img src="../assets/white-logo.png" alt="logo-mediblabla"></a>
+        <a href=""><img src="../assets/white-logo.png" alt="logo-mediblabla" class="logo-mediblabla"></a>
         <p class="mt-5">
             Copyright - Design <span class="font-bold">Mediblabla</span>
         </p>
@@ -15,5 +15,14 @@
 
 
 <style>
-
+    @media screen and (max-width: 768px){
+            .logo-mediblabla {
+                height: 30px;
+                text-align: center;
+            }
+            
+            p {
+                font-size: 12px;
+            }
+        }
 </style>
