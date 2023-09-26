7358eec3aeb8cff045185021e5b73cdf6f7fe984
Lotshana <nahh.design@gmail.com>
2023-09-26 16:27:38 +0200

    Modification formulaire

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -20,10 +20,10 @@
                 <p v-if="errorMessage" class="(error A VOIR AVEC TAILWIND)"></p>
 
                 <!-- Form Group -->
-                <div>
+                <div v-if="mode == 'create'">
                   <label for="email" class="block text-sm mb-2 text-darkblue">Pseudo</label>
                   <div class="relative">
-                    <input  type="email" id="email" v-model="email" name="email" class="py-3 px-4 block w-full bg-lightgrey border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="email-error">
+                    <input type="" id="pseudo" v-model="pseudo" name="pseudo" class="py-3 px-4 block w-full bg-lightgrey border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="email-error">
                   </div>
                   <p class="hidden text-xs text-red-600 mt-2" id="email-error">Merci d'entrer une adresse mail valide s'il vous plait</p>
                 </div>
@@ -49,6 +49,23 @@
                   <div class="flex justify-between items-center">
                     <label for="password" class="block text-sm mb-2 text-darkblue">Mot de passe</label>
                     <a class="text-sm text-blue-600 decoration-2 hover:underline font-medium" href="../examples/html/recover-account.html">Mot de passe oublié ?</a>
+                  </div>
+                  <div class="relative">
+                    <input type="password" v-model="password" id="password" name="password" class="py-3 px-4 block w-full  bg-lightgrey border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="password-error">
+                    <div class=" absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                      <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                      </svg>
+                    </div>
+                  </div>
+                  <p class="hidden text-xs text-red-600 mt-2" id="password-error">8 caractères minimum</p>
+                </div>
+                <!-- End Form Group -->
+    
+                 <!-- Form Group -->
+                 <div v-if="mode == 'create'">
+                  <div class="flex justify-between items-center">
+                    <label for="password" class="block text-sm mb-2 text-darkblue">Confirmation du mot de passe</label>
                   </div>
                   <div class="relative">
                     <input type="password" v-model="password" id="password" name="password" class="py-3 px-4 block w-full  bg-lightgrey border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="password-error">
