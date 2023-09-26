b0ee6d2abc239a2dd816d088a0420f85a185fd57
AlexBdx01 <badoux.al@gmail.com>
2023-09-26 10:02:12 +0200

    Update CreaArt.vue

--- src/views/CreaArt.vue
+++ src/views/CreaArt.vue
@@ -38,7 +38,7 @@
 </div>
 
 <div class="relative">
-    <label for="" class="block text-sm text-darkblue font-medium">tags</label>
+    <label for="" class="block text-sm text-darkblue font-medium">Tags</label>
     <input type="text" class="py-3 px-4 block w-full  bg-lightgrey border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">
 </div> 
 
