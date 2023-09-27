412328bd64725126cc733dd320d807fdcc9f2cfc
Inuokko <leajoussaume64@gmail.com>
2023-09-27 16:42:20 +0200

    ALED

--- src/views/Creationarticle.vue
+++ src/views/Creationarticle.vue
@@ -5,11 +5,11 @@
 
     const submitForm = async () => {
         const instance = axios.create({
-            baseURL: "http://localhost/Php_Mediblabla/article/",
+            baseURL: "http://localhost/Php_mediblabla/article/",
             headers: {
                 'Content-Type': 'multipart/form-data',
             },
-            withCredentials: true,
+            //withCredentials: true,
         });
 
         try {
@@ -72,7 +72,7 @@
                 </div>
 
                 <button type="submit"
-                        class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-beige text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">Connexion</button>
+                        class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-beige text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">Ajouter</button>
             </div>
         </form>
     </main>
